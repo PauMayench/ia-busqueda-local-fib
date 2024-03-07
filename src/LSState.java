8c3023edd61a1b0b9b6545fd61373e155e7b9056
josep.diaz.sosa <josep.diaz.sosa@estudiantat.upc.edu>
2024-03-07 13:07:21 +0100

    Primer algorisme Greedy per a la generació de solució inicial a l'espai de solució

--- src/LSState.java
+++ src/LSState.java
@@ -1,3 +1,6 @@
+import IA.DistFS.Requests;
+import IA.DistFS.Servers;
+
 public class LSState {
 
 
@@ -9,12 +12,41 @@ public class LSState {
    * 
    * @param [in]
    */
-  public LSState() {
 
+  private static Requests requests;
+  private static Servers servers;
+  private int [] totalTimeServers;
+  private int [] serverRequests;
+
+  //Inicialitza
+  public static void InitializeStatic(Requests req, Servers serv) {
+    requests = req;
+    servers = serv;
   }
-  
 
- 
+  //Crea solució incial Greedy temps total mínim.
+  public LSState() {
+    int numServers = servers.size();
+    int numRequests = requests.size();
+    totalTimeServers = new int[numServers];
+    serverRequests = new int[numRequests];
+    for (int i = 0; i < numServers; ++i) totalTimeServers[i] = 0;
+    for (int i = 0; i < numRequests; ++i) {
+      int[] actualRequest = requests.getRequest(i);
+      int userId = actualRequest[0];
+      int minTime = -1;
+      int minServer = -1;
+      for (int j = 0; j < numServers; ++j) {
+        int actualTime = servers.tranmissionTime(j,userId);
+        if ((actualTime < minTime) || (minTime == -1)) {
+          minTime = actualTime;
+          minServer = j;
+        }
+      }
+      serverRequests[i] = minServer; //Assignem request al sevidor minServer.
+      totalTimeServers[minServer] += minTime; //Afegim al total del servidor el temps de la nova request.
+    }
+  }
 }
 
 
