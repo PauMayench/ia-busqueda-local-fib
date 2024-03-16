1ca02d7b1f67fb341d96371fc93455857d841c1d
josep.diaz.sosa <josep.diaz.sosa@estudiantat.upc.edu>
2024-03-16 20:31:41 +0100

    Les 2 funcions heuristiques implementades

--- src/LSHeuristicFunction1.java
+++ src/LSHeuristicFunction1.java
@@ -1,10 +1,18 @@
 import aima.search.framework.HeuristicFunction;
 
+/*CRITERI 1: Minimización del tiempo de transmisión de los ficheros para el servidor que necesita más tiempo para
+transmitir sus peticiones.*/
 public class LSHeuristicFunction1 implements HeuristicFunction  {
-
-  public double getHeuristicValue(Object state) {
-
-      return (1.0);
-  }
+    public double getHeuristicValue(Object state) {
+        LSState estatActual = (LSState)state;
+        int[] totalTimeServers = estatActual.getTotalTimeServers();
+        int nElems = totalTimeServers.length;
+        int max = totalTimeServers[0];
+        for(int i = 1; i < nElems; ++i) {
+            int actual = totalTimeServers[i];
+            if (actual > max) max = actual;
+        }
+        return max * max; //Quadrat per a augmentar les diferències entre valors.
+    }
   
 }
--- src/LSHeuristicFunction2.java
+++ src/LSHeuristicFunction2.java
@@ -1,10 +1,30 @@
 import aima.search.framework.HeuristicFunction;
 
+/*
+CRITERI 2: Minimizar el tiempo total de transmisión de los ficheros (suma de todos los tiempos de transmisión),
+pero con la restricción de que los tiempos de transmisión de los servidores han de ser lo más similares
+posibles entre ellos.
+*/
 public class LSHeuristicFunction2 implements HeuristicFunction  {
 
     public double getHeuristicValue(Object state) {
+        LSState estatActual = (LSState) state;
+        // Càlcul de la mitjana.
+        int[] totalTimeServers = estatActual.getTotalTimeServers();
+        int sum = 0;
+        int mida = totalTimeServers.length;
+        for (int i : totalTimeServers) sum += i;
+        double average = (double)sum / mida;
 
-        return (1.0);
+        // Càlcul de les desviacions de cada servidor respecte la mitjana.
+        double variance = 0.0;
+        for (int serverTime : totalTimeServers) {
+            double v = serverTime - average;
+            variance += (v*v);
+        }
+
+        // PREGUNTA: RETORNAR VARIANCE O VARIANCE + SUM???
+        return (variance); // Quadrat de la desviació respecte a la mitjana de temps entre tots els servidors.
     }
 
 }
